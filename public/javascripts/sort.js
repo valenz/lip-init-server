b62bc7bb9945c4e97f62698e1e88f22b987b2cda
Stephan Wegner <stephan.wegner@mail.de>
2014-09-28 22:04:34 +0200

    Playing with localStorage

--- public/javascripts/sort.js
+++ public/javascripts/sort.js
@@ -7,29 +7,80 @@ $(document).ready(function() {
 			stop: handleDragStop
 		});
 		
-		var grid = new Object();
+		//localStorage.setItem('grid', JSON.stringify(init()));
+		var orig = init();
 		
 		
 		
-		if(localStorage.getItem('modified')) {
+		if(localStorage.getItem('modified') == 'true') {
 			// TODO
 			// Update each tab individually
 			
 			/** Get local storage */
-			var item = JSON.parse(localStorage.getItem('grid'))
+			var self = JSON.parse(localStorage.getItem('self')),
+				//orig = JSON.parse(localStorage.getItem('grid')),
 				str = '';
-			for(var i in item) {
-				for(var j in item[i]) {
-					str += item[i][j];
+			update(orig, self);
+			for(var sort1 in self) {
+				for(var tab1 in self[sort1]) {
+					str += self[sort1][tab1];
 				}
 			}
 			
 			$('#grid').html(str);
-		} else {
-			init();
 		}
 		
 		
+		
+		function update(x, y) {
+			var grid = new Object();
+			var a = new Array();
+			var b = new Array();
+			var xProps = Object.getOwnPropertyNames(x);
+			var yProps = Object.getOwnPropertyNames(y);
+			console.log(x);
+			console.log(y);
+			
+			for(var i = 0; i < yProps.length; i++) {
+				var propName = yProps[i];
+				
+				console.log(y[propName]);
+				
+				for(var key in y[propName]) {
+					b.push(key);
+				}
+				
+				for(var key in x[propName]) {
+					a.push(key);
+				}
+				
+				for(var j in b) {
+					for(var k in a) {
+						if(b[j] == a[k]) {
+							console.log(k);
+						}
+					}
+				}
+				
+				if(y[propName] !== x[propName]) {
+					//console.log(false);
+				}
+			}
+			
+			console.log(a);
+			
+			//localStorage.setItem('test', JSON.stringify(grid));
+			
+			/*for(p in x, y) {
+				for(q in x[p], y[p]) {
+					$('li.tabs').each(function(key, value) {
+						if($(this).find('[type=hidden]').attr('value') == q) {
+							console.log(key);
+						}
+					});
+				}
+			}*/
+		}
 		
 		function handleDragStart(event, ui) {
 			// TODO
@@ -37,18 +88,19 @@ $(document).ready(function() {
 		}
 		
 		function handleDragStop(event, ui) {
-			init();
+			localStorage.setItem('self', JSON.stringify(init()));
 			localStorage.setItem('modified', true);
 		}
 		
 		/** Initialize default grid */
 		function init() {
+			var grid = new Object();
 			$('li.tabs').each(function(key, value) {
 				var tab = new Object();
 				tab[$(this).find('[type=hidden]').attr('value')] = value.outerHTML;
 				grid[key] = tab;
 			});
-			localStorage.setItem('grid', JSON.stringify(grid));
+			return grid;
 		}
 	}
 });
