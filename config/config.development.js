3e8ee1babb9db0e43976b99aa340cf007d1b2b23
Stephan Wegner <wegner@mpib-berlin.mpg.de>
2015-02-15 00:58:10 +0100

    Updated: development log level

--- config/config.development.js
+++ config/config.development.js
@@ -3,5 +3,6 @@ var config = require('./config.global');
 // Overrides global config (run Node with NODE_ENV=development)
 
 config.env = 'development';
+config.loggers.log.console.level = 'debug';
 
 module.exports = config;
